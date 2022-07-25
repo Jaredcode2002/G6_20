64d872815cd52b33099b09c65a104428f2910c09
Jared <Jared@JARED>
2022-07-25 17:26:00 -0600

    Primer commit con la entidad vuelo casi terminada

--- Vuelos/controller/vuelo.php
+++ Vuelos/controller/vuelo.php
@@ -12,7 +12,7 @@
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
 
-    require_once("../config/conexion.php");
+    require_once("../../config/conexion.php");
     require_once("../models/vuelo.php");
 
     $vuelo = new vuelo();
